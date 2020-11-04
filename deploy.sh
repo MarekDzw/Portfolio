8648d91794dcf2ad0892bc9cc92d33d70e89bdbe
Marek Dzwonnik <33802916+MarekDzw@users.noreply.github.com>
2020-11-04 13:07:58 +0100

    change

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:marekdzw/Portfolio.git master:gh-pages
+git push -f git@github.com:marekdzw/Portfolio-Vue.git master:gh-pages
 
 cd -
