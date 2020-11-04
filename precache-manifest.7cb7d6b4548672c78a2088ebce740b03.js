91df20f43775e7dc56ed0d71ba75bfa841a9c5d9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-04 14:49:42 +0000

    Deploy MarekDzw/Portfolio-Vue to github.com/MarekDzw/Portfolio-Vue.git:gh-pages

---
+++ precache-manifest.7cb7d6b4548672c78a2088ebce740b03.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f457ba2cdac514a4ddbc",
+    "url": "/Portfolio-Vue/css/app.0c1fe962.css"
+  },
+  {
+    "revision": "a1cd0ce18e0c0f39d2f63c8e13da5588",
+    "url": "/Portfolio-Vue/icon.png"
+  },
+  {
+    "revision": "9d696805f9ccf2f2a852d6c67865ba67",
+    "url": "/Portfolio-Vue/img/blog.9d696805.png"
+  },
+  {
+    "revision": "bcecf23b8186299cf87cfb01c5239c59",
+    "url": "/Portfolio-Vue/img/greathouse.bcecf23b.png"
+  },
+  {
+    "revision": "2c8cb148f6e58ef075f0f8d3cfd2f439",
+    "url": "/Portfolio-Vue/img/grudzien.2c8cb148.png"
+  },
+  {
+    "revision": "23d2f13f3bc9364ff3d453e9218e991f",
+    "url": "/Portfolio-Vue/img/grudzienv1.23d2f13f.png"
+  },
+  {
+    "revision": "86b4546042300ff6252280c6a09dc033",
+    "url": "/Portfolio-Vue/img/ind.86b45460.png"
+  },
+  {
+    "revision": "0e47d21ac4256221a35203c753660b87",
+    "url": "/Portfolio-Vue/img/klosinska.0e47d21a.png"
+  },
+  {
+    "revision": "d7513fa0d9cb90929c082dba69b1ba06",
+    "url": "/Portfolio-Vue/img/komp.d7513fa0.png"
+  },
+  {
+    "revision": "10842aca3ebe4f7ad9a6a8f5c5bd6fe0",
+    "url": "/Portfolio-Vue/img/kontakt.10842aca.png"
+  },
+  {
+    "revision": "1cc7c5a81602cac8169a717c29c265ed",
+    "url": "/Portfolio-Vue/img/logo.1cc7c5a8.svg"
+  },
+  {
+    "revision": "296be753f92bd3278eda8b3a6bf9cdd4",
+    "url": "/Portfolio-Vue/img/marek.296be753.png"
+  },
+  {
+    "revision": "24ac8c1b9dd87b1be47fa6d3206faf34",
+    "url": "/Portfolio-Vue/img/pages.24ac8c1b.png"
+  },
+  {
+    "revision": "3dab9faeb483236a38879fa50f893a49",
+    "url": "/Portfolio-Vue/index.html"
+  },
+  {
+    "revision": "f457ba2cdac514a4ddbc",
+    "url": "/Portfolio-Vue/js/app.d9cac407.js"
+  },
+  {
+    "revision": "fc291518fa27cbe60a93",
+    "url": "/Portfolio-Vue/js/chunk-vendors.8f0b0301.js"
+  },
+  {
+    "revision": "cd94e0936cfe1a0e01fb3f6323c97bad",
+    "url": "/Portfolio-Vue/manifest.json"
+  }
+]);
